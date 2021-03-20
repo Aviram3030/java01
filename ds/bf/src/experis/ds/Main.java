4675372ed6c14241c329d15381df70abc50cc896
unknown <aviram3030@gmail.com>
2021-03-20 23:50:12 +0200

    fixing if in main

--- ds/bf/src/experis/ds/Main.java
+++ ds/bf/src/experis/ds/Main.java
@@ -19,16 +19,16 @@ public class Main {
                 "       !";
         source = Emulator.fixing(source);
 
-        if(!Emulator.checkIllegalBrackets(source)){
-            System.out.println("Error, illegal brackets");
-            return;
-        }
-
         calculate(source);
 
     }
 
     public static void calculate(String source){
+        if(!Emulator.checkIllegalBrackets(source)){
+            System.out.println("Error, illegal brackets");
+            return;
+        }
+
         OpCodes[] binaryCode = Emulator.turnToOpCode(source);
         Display display = Display.getDisplay();
         Emulator test = new Emulator(display);
