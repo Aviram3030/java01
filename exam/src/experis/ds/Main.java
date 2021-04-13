18c24f76993734cc83eb685c35feba9ff948fd14
unknown <aviram3030@gmail.com>
2021-04-13 13:33:23 +0300

    checking something

--- exam/src/experis/ds/Main.java
+++ exam/src/experis/ds/Main.java
@@ -1,12 +1,7 @@
 package experis.ds;
 
-import java.util.HashMap;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        int[] arr = new int[]{7,7,1,2,1,2,8,8};
-        System.out.println(FirstQuestion.f1(arr));
     }
 }
