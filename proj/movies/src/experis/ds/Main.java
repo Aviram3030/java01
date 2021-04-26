11e59346278cf4955a8f93a1c64c8c0e7f5f78b6
unknown <aviram3030@gmail.com>
2021-04-26 11:38:48 +0300

    inital commit, learning json

---
+++ proj/movies/src/experis/ds/Main.java
@@ -0,0 +1,80 @@
+package experis.ds;
+
+import org.json.simple.parser.ParseException;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+class ParameterStringBuilder {
+    public static String getParamsString(Map<String, String> params)
+            throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            result.append("&");
+        }
+
+        String resultString = result.toString();
+        return resultString.length() > 0
+                ? resultString.substring(0, resultString.length() - 1)
+                : resultString;
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
+        URL url = new URL("http://www.omdbapi.com/?i=tt3896198&apikey=b31ba527&t=harry");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("param1", "val");
+
+        con.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(con.getOutputStream());
+        out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
+        out.flush();
+        out.close();
+
+
+
+        /*URL url = new URL("http://www.omdbapi.com/?apikey=6c823ea6");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+
+        int responseCode = conn.getResponseCode();
+        if (responseCode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + responseCode);
+        } else {
+
+            String inline = "";
+            Scanner scanner = new Scanner(url.openStream());
+
+            while (scanner.hasNext()) {
+                inline += scanner.nextLine();
+            }
+
+            scanner.close();
+
+            JSONParser parse = new JSONParser();
+            JSONObject data_obj = (JSONObject) parse.parse(inline);
+            JSONObject obj = (JSONObject) data_obj.get("Global");
+
+            System.out.println(obj.get("TotalRecovered"));
+        }*/
+
+    }
+}
