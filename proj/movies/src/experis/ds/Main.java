47191318a15106d1879ebcb691ae123d353d0a97
unknown <aviram3030@gmail.com>
2021-04-27 10:46:10 +0300

    adding user interface

--- proj/movies/src/experis/ds/Main.java
+++ proj/movies/src/experis/ds/Main.java
@@ -1,64 +1,10 @@
 package experis.ds;
 
 
-import com.google.gson.Gson;
-import experis.ds.gson.Movie;
-import experis.ds.gson.TitleQueryResult;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
-
 public class Main {
 
-    public static void URL(String s) throws IOException {
-        Gson gson = new Gson();
-        URL url = new URL(s);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-
-        con.connect();
-        int responseCode = con.getResponseCode();
-        Scanner sc = new Scanner(url.openStream());
-        StringBuilder inline = new StringBuilder();
-        while(sc.hasNext())
-        {
-            inline.append(sc.nextLine());
-        }
-        System.out.println("\nJSON data in string format");
-        System.out.println(inline);
-
-        TitleQueryResult result = gson.fromJson(inline.toString(), TitleQueryResult.class);
-        System.out.println();
-        String id = result.getSearch()[2].getImdbID();
-
-        URLForId(id);
-    }
-
-    private static void URLForId(String s) throws IOException {
-        Gson gson = new Gson();
-        URL url = new URL("http://www.omdbapi.com/?apikey=b31ba527&i="+ s +"&");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-
-        con.connect();
-        int responseCode = con.getResponseCode();
-        Scanner sc = new Scanner(url.openStream());
-        StringBuilder inline = new StringBuilder();
-        while(sc.hasNext())
-        {
-            inline.append(sc.nextLine());
-        }
-
-        Movie result = gson.fromJson(inline.toString(), Movie.class);
-        System.out.println();
-    }
-
-    public static void main(String[] args) throws IOException {
-        URL("http://www.omdbapi.com/?apikey=b31ba527&s=star+wars&");
-        //URL("http://www.omdbapi.com/?apikey=b31ba527&i=tt0080684&", "Title");
-
+    public static void main(String[] args){
+        UserInterface.start();
     }
 }
 
--- proj/movies/src/experis/ds/UserInterface.java
+++ proj/movies/src/experis/ds/UserInterface.java
@@ -1,4 +1,24 @@
 package experis.ds;
 
+import experis.ds.gson.Movie;
+
+import java.util.Scanner;
+
 public class UserInterface {
+    public static void start(){
+        Scanner reader = new Scanner(System.in);
+        var movieCenter = MovieCenter.getInstance();
+        System.out.print("Hello, Which movie are you looking for? ");
+        while(true){
+            System.out.println("\ntype '.' to exit");
+            System.out.print("> ");
+            String input = reader.nextLine();
+            if(input.equals(".")){
+                break;
+            }
+            Movie[] movies = movieCenter.search(input);
+            Display.print(movies);
+        }
+        System.out.println("Goodbye");
+    }
 }
