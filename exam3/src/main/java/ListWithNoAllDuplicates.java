233541ec19f5fb7aec5e2f72b06cf33e05c3b581
unknown <aviram3030@gmail.com>
2021-05-23 22:37:27 +0300

    add second version of delete duplicates

---
+++ exam3/src/main/java/ListWithNoAllDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class ListWithNoAllDuplicates {
+    public static <T> void deleteAllDuplicates(LinkedList<T> list){
+        HashMap<T, Boolean> noDuplicates = new HashMap<>();
+        int i = 0;
+        while(i < list.size()){
+            T element = list.get(i);
+            if(noDuplicates.containsKey(element)){
+                if(!noDuplicates.get(element)) {
+                    noDuplicates.put(element, true);
+                    list.remove(element);
+                }
+                list.remove(element);
+            }
+            else{
+                noDuplicates.put(element, false);
+                i++;
+            }
+        }
+    }
+}
